c074e66166518fd3fb3ce43eb070d8203e2d661b
Nilay Vishwakarma <nivishwa@microsof.com>
2025-02-12 22:17:30 +0530

    version upgrade, update vulnerable componenets

--- SharpWiki.Tests/MediaTests.cs
+++ SharpWiki.Tests/MediaTests.cs
@@ -2,5 +2,20 @@ namespace SharpWiki.Tests
 {
     public class MediaTests
     {
+        [Theory]
+        [InlineData("Jupiter")]
+        public void CanGetFilesOnPage(string pageName)
+        {
+            using SharpWikiClient client = new SharpWikiClient(new SharpWikiClientOptions
+            {
+                ApiUserAgent = "SharpWiki.Tests",
+                Language = Models.WikiLanguage.English,
+                GetToken = () =>
+                {
+                    return "";
+                }
+            });
+            var files = client.GetFilesOnPageAsync(pageName).GetAwaiter().GetResult();
+        }
     }
 }
